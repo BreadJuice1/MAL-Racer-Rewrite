e92a7d4e3955bbe912e046000c2957bb5ce0fb81
breadjuice1 <austinjf@protonmail.com>
2021-10-04 15:32:23 -0700

    added getUsername()

--- tracker.py
+++ tracker.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+def getUsername(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    username = soup.findAll()
+    return username
+
 def statChecker(url):
-    parsedStats = [url[32:].lower()]
+    parsedStats = [getUsername(url)]
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     rawStats = soup.findAll('div', attrs={"class":"di-tc al pl8 fs12 fw-b"})
