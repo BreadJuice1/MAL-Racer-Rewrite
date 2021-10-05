a8e310bbb5d2f63258e0f127e109394cc3d4f70c
BreaJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2021-10-04 17:49:22 -0700

    s'good for now

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import csv
+import matplotlib.pyplot as plt
 from tracker import statChecker
 
-usr = input("Enter Username:")
-print(statChecker("https://myanimelist.net/profile/"+usr))
+partPants = []
+cur = []
+partPS = []
+
+with open('participants.csv') as partPants:
+    partPants = csv.reader(partPants)
+
+    for row in partPants:
+        tempP = statChecker(row[0])
+        cur.append(float(row[2])-(float(row[1])-tempP[2]))
+        partPS.append(tempP[0])
+
+plt.pie(cur, labels = partPS, autopct = '%.1f%%')
+plt.show()
--- tracker.py
+++ tracker.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def getUsername(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    username = soup.findAll()
-    return username
+    username = soup.find('title')
+    return username.text[1:-29]
 
 def statChecker(url):
+    url = 'https://myanimelist.net/profile/'+url
     parsedStats = [getUsername(url)]
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     rawStats = soup.findAll('div', attrs={"class":"di-tc al pl8 fs12 fw-b"})
     for title in rawStats:
         parsedStats.append(float(title.text[6:]))
-    return parsedStats
+    return parsedStats # list formatted like so: ['username', anime days (float), manga days (float)]
