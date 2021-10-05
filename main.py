940e01e18f2bbd00986b3aaeada77105840c42f9
BreaJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2021-10-04 18:06:42 -0700

    beautification

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ with open('participants.csv') as partPants:
         cur.append(float(row[2])-(float(row[1])-tempP[2]))
         partPS.append(tempP[0])
 
-plt.pie(cur, labels = partPS, autopct = '%.1f%%')
+plt.ylim(0,8.5)
+plt.title('The Great Manga Race')
+plt.xlabel('Participants')
+plt.ylabel('Days Read Since Start of Race')
+plt.bar(partPS, cur)
+# plt.pie(cur, labels = partPS, autopct = '%.1f%%')
 plt.show()
